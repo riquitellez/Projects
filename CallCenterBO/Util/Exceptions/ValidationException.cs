fc4202752363856112d97cfe48b18e62cf48084d
riquitellez <54512082+riquitellez@users.noreply.github.com>
2019-08-25 22:18:52 +0200

    Add files via upload

---
+++ CallCenterBO/Util/Exceptions/ValidationException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CallCenterBO.Util.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        private string _validationError;
+
+        public ValidationException(string mensaje) : base(mensaje)
+        {
+            _validationError = mensaje;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - Detalles: {1}", _validationError, base.ToString());
+        }
+    }
+}
